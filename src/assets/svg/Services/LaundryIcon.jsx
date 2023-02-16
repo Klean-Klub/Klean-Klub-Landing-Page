9f63c57fb76ae7367585ae2f18d2297f92541c41
carzard707 <86026238+carzard707@users.noreply.github.com>
2023-02-16 13:26:16 +0000

    Initial commit

---
+++ src/assets/svg/Services/LaundryIcon.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+function SvgComponent(props) {
+  return (
+<svg width="104" height="104" viewBox="0 0 104 104" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_1_3715)">
+<circle cx="52" cy="40" r="40" fill="#ECF1FF"/>
+</g>
+<path d="M48.4625 46.0375C49.4016 46.9739 50.6738 47.4998 52 47.4998C53.3262 47.4998 54.5984 46.9739 55.5375 46.0375C56.4739 45.0984 56.9998 43.8262 56.9998 42.5C56.9998 41.1738 56.4739 39.9016 55.5375 38.9625L48.4625 46.0375ZM59.5 27.5125L44.5 27.5C43.1125 27.5 42 28.6125 42 30V50C42 51.3875 43.1125 52.5 44.5 52.5H59.5C60.8875 52.5 62 51.3875 62 50V30C62 28.6125 60.8875 27.5125 59.5 27.5125ZM49.5 30C50.1875 30 50.75 30.5625 50.75 31.25C50.75 31.9375 50.1875 32.5 49.5 32.5C48.8125 32.5 48.25 31.9375 48.25 31.25C48.25 30.5625 48.8125 30 49.5 30ZM45.75 30C46.4375 30 47 30.5625 47 31.25C47 31.9375 46.4375 32.5 45.75 32.5C45.0625 32.5 44.5 31.9375 44.5 31.25C44.5 30.5625 45.0625 30 45.75 30ZM52 50C47.8625 50 44.5 46.6375 44.5 42.5C44.5 38.3625 47.8625 35 52 35C56.1375 35 59.5 38.3625 59.5 42.5C59.5 46.6375 56.1375 50 52 50Z" fill="#4C5572"/>
+<defs>
+<filter id="filter0_d_1_3715" x="0" y="0" width="104" height="104" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feMorphology radius="8" operator="erode" in="SourceAlpha" result="effect1_dropShadow_1_3715"/>
+<feOffset dy="12"/>
+<feGaussianBlur stdDeviation="10"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0.501961 0 0 0 0 0.501961 0 0 0 0 0.501961 0 0 0 0.26 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_3715"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_3715" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+
+  );
+}
+
+export default SvgComponent;
